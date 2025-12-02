7cccf6ad6b0f5765dca09e200c71be4a6dec83f6
Daniel <daniel@hoolihan.org>
2025-12-02 18:10:22 +1000

    day2 done

--- day_02.py
+++ day_02.py
@@ -2,15 +2,13 @@ import sys,os,re,itertools
 from utils import *
 
 t=0
-rawdata = [r.strip().split(',') for r in open('day_02.txt','rt').readlines()]
+rawdata = [r.strip().split(',') for r in open('inputs/day_02.txt','rt').readlines()]
 for row in rawdata[0]:
-  print(row)
   fr,to = row.split('-')
   for i in range(int(fr),int(to)+1):
     n = str(i)
-    if n[:len(n)//2] == n[len(n)//2:]: 
-      print(i,'dup')
+    if re.fullmatch(r'(.+)\1+',n):
+    # if n[:len(n)//2] == n[len(n)//2:]: 
       t+=i
-    else: print(i)
 
 print(t)
