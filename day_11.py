dede2a0a23161d7355511fa9da23709cff6a95f3
daniel-hoolihan_eql <daniel.hoolihan@energyq.com.au>
2025-12-16 09:11:42 +1000

    day11 - borrowed

--- day_11.py
+++ day_11.py
@@ -1,29 +1,36 @@
 import sys,os,re,itertools,functools
 from utils import *
 
-world = {row[0]: row[1].strip().split(' ') for row in [r.strip().split(':') for r in open('inputs/day_11.txt','rt').readlines()]}
+world = {row[0]: row[1].strip().split() for row in [r.strip().split(':') for r in open('inputs/day_11.txt','rt').readlines()]}
+visited = set()
 tally = 0
+
+@functools.cache
+def dfc(node, fft, dac):
+  if node == 'out':
+      return 1 if fft and dac else 0
+  if (outputs := world.get(node)) is None:
+      return 0
+  if node == 'fft': fft = True
+  if node == 'dac': dac = True
+  return sum(dfc(out, fft, dac) for out in outputs)
 
 @functools.cache
 def dfs(path,node,tgt):
   global tally
   path += node
-  if node == tgt:    
-    tally+=1
+  if node == tgt:
+    tally += 1
     return
   links = world.get(node,[])
   for l in links:
     dfs(path,l,tgt)
-    path = path[:-1]
 
 dfs('','you','out')
 print(tally)
 
-dfs('','svr','ffs')
+tally = dfc('you',True,True)
 print(tally)
 
-dfs('','ffs','dac')
-print(tally)
-
-dfs('','dac','out')
-print(tally)
+tot = dfc('svr', False, False)
+print(tot)
